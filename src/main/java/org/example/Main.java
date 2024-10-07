96fe621a1b9d922da7cc0e78b8e3ea8e22f73f52
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-10-07 13:28:21 +0200

    exercise 2 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,9 @@ public class Main {
                     case 1:
                         exercise1();
                         break;
+                    case 2:
+                        exercise2();
+                        break;
                     default:
                         break;
                 }
@@ -23,6 +26,35 @@ public class Main {
                 System.out.println("Invalid exercise. Closing program");
                 System.exit(0);
             }
+        }
+    }
+    public static void exercise2(){
+        System.out.println("Creating a program that take a year from user and checks if its a leap year or not.");
+        System.out.println();
+        var scan = new Scanner(System.in);
+        System.out.println("Enter a year: ");
+        try{
+            int year = scan.nextInt();
+            if (year % 100 == 0){
+                if (year % 400 == 0){
+                    System.out.println("Year " + year + " is a century and a leap year.");
+                }
+                else{
+                    System.out.println("Year " + year + " is a century and Not a leap year.");
+                }
+            }
+            else{
+                if (year % 4 == 0){
+                    System.out.println("Year " + year + " is a leap year.");
+                }
+                else{
+                    System.out.println("Year " + year + " is Not a leap year.");
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println("Bad year, closing program.");
+            System.exit(0);
         }
     }
 
